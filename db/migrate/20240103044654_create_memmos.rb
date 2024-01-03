0b7c8731dec2a19e2451a7e4072ef300961648d7
K-Yaman0 <tezza10yama@gmail.com>
2024-01-03 13:49:25 +0900

    カラムを追加

--- db/migrate/20240103044654_create_memmos.rb
+++ db/migrate/20240103044654_create_memmos.rb
@@ -1,7 +1,7 @@
 class CreateMemmos < ActiveRecord::Migration[7.0]
   def change
     create_table :memmos do |t|
-
+      t.string :memo,          null: false
       t.timestamps
     end
   end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_12_27_120634) do
+ActiveRecord::Schema[7.0].define(version: 2024_01_03_044654) do
+  create_table "memmos", charset: "utf8", force: :cascade do |t|
+    t.string "memo", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "tasks", charset: "utf8", force: :cascade do |t|
     t.string "content", null: false
     t.datetime "created_at", null: false
