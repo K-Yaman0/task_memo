7bffb4fab3b94ca37d881349db7a8b377a58928f
K-Yaman0 <tezza10yama@gmail.com>
2024-01-05 11:38:07 +0900

    メモ追加用フォームを配置

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2024_01_03_045302) do
+ActiveRecord::Schema[7.0].define(version: 2024_01_05_023618) do
   create_table "memos", charset: "utf8", force: :cascade do |t|
     t.string "memo", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "user_id"
+    t.index ["user_id"], name: "index_memos_on_user_id"
   end
 
   create_table "tasks", charset: "utf8", force: :cascade do |t|
@@ -38,5 +40,6 @@ ActiveRecord::Schema[7.0].define(version: 2024_01_03_045302) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "memos", "users"
   add_foreign_key "tasks", "users"
 end
