7bb3c71cc3fab80a005f8aad80b424365b1e1081
K-Yaman0 <tezza10yama@gmail.com>
2024-01-05 11:44:44 +0900

    メモを投稿・保存できるよう記述

--- app/controllers/memos_controller.rb
+++ app/controllers/memos_controller.rb
@@ -1,18 +1,18 @@
 class MemosController < ApplicationController
   def create
-    @comment = Comment.create(comment_params)
-    if @comment.save
-      redirect_to prototype_path(@comment.prototype)
+    @memo = Memo.create(memo_params)
+    if @memo.save
+      redirect_to task_path(@memo.task)
     else
-      @prototype = @comment.prototype
-      @comments = @prototype.comments
-      render 'prototypes/show', status: :unprocessable_entity
+      @task = @memo.task
+      @memos = @task.memos
+      render 'tasks/show', status: :unprocessable_entity
     end
   end
 
   private
 
-  def comment_params
-    params.require(:comment).permit(:content).merge(user_id: current_user.id, prototype_id: params[:prototype_id])
+  def memo_params
+    params.require(:memo).permit(:memo).merge(user_id: current_user.id, task_id: params[:task_id])
   end
 end
