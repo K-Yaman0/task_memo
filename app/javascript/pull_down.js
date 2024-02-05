7e6e97b4264bd43f47e6626defe31fd0ba0de08d
K-Yaman0 <tezza10yama@gmail.com>
2024-02-05 14:30:43 +0900

    重複する記述をまとめる編集

--- app/javascript/pull_down.js
+++ app/javascript/pull_down.js
@@ -1,6 +1,7 @@
 function pullDown() {
   const pullDownButtons = document.querySelectorAll("#pull-down-lists");
   const pullDowns = document.querySelectorAll(".hidden");
+  const showPullDowns = document.querySelectorAll(".memo-hidden");
 
   pullDownButtons.forEach(function(button, index) {
     button.addEventListener('click', function() {
@@ -14,12 +15,28 @@ function pullDown() {
         currentPullDown.style.display = "block";
       }
     });
+
+    button.addEventListener('click', function() {
+      const currentPullDown = showPullDowns[index];
+      if (currentPullDown.style.display === "block") {
+        currentPullDown.style.display = "none";
+      } else {
+        showPullDowns.forEach(function(pullDown) {
+          pullDown.style.display = "none";
+        });
+        currentPullDown.style.display = "block";
+      }
+    });
   });
 
   window.addEventListener('click', function(event) {
     const clickedElement = event.target;
     if (!Array.from(pullDownButtons).includes(clickedElement))  {
       pullDowns.forEach(function(pullDown) {
+        pullDown.style.display = "none";
+      });
+
+      showPullDowns.forEach(function(pullDown) {
         pullDown.style.display = "none";
       });
     }
